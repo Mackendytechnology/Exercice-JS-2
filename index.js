4f129b0ddc830d63e2ef6b403c6ee3b33f46f268
MackendyTechnology <bouquetmackendy34@gmail.com>
2023-10-09 17:35:31 -0400

    Exercice-JS-2

---
+++ index.js
@@ -0,0 +1,29 @@
+function returnString(string) {
+    return string;
+ }
+ console.log(returnString('Mackendy'));
+ 
+ function sum(x, y) {
+    return x + y;
+ }
+ console.log(sum(5, 3));
+ 
+ function sub(x, y) {
+    return x - y;
+ }
+ console.log(sub(10, 4));
+ 
+ function divide(x, y) {
+    return x / y;
+ }
+ console.log(divide(20, 5));
+ 
+ function multiply(x, y) {
+    return x * y;
+ }
+ console.log(multiply(6, 7));
+ 
+ function getRest(x, y) {
+    return x % y;
+ }
+ console.log(getRest(21, 5));
